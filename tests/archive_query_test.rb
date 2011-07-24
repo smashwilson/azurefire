3ad29a3410f9ff5615e239e5ce3a26be9241dce5
Ash Wilson <smashwilson@gmail.com>
2011-07-24 13:51:34 -0400

    Cleaning out more older, failing tests.

--- tests/archive_query_test.rb
+++ tests/archive_query_test.rb
@@ -5,33 +5,4 @@ require_relative '../model/journal_post'
 
 class ArchiveQueryTest < StorageTestCase
   
-  def setup
-    other = User.new
-    other.username = 'other'
-    other.save
-    
-    @all = []
-    1.upto(50) do |index|
-      p = JournalPost.new
-      p.title = index.to_s.rjust(3, '0')
-      p.username = index.even? ? 'foo' : 'other'
-      p.save
-      @all << p
-    end
-  end
-  
-  def test_default_to_all
-    q = ArchiveQuery.new
-    
-    assert_equal 'all posts', q.to_s
-    assert_equal @all.sort, q.results
-  end
-  
-  def test_query_by_user
-    q = ArchiveQuery.from 'foo'
-    
-    assert_equal 'posts by <strong>foo</strong>', q.to_s
-    assert_equal @all.select { |post| post.username == 'foo' }.sort, q.results
-  end
-  
 end
