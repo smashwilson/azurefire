097efe2c398146981e5eb3b7a43b5aed51837e94
Ash Wilson <smashwilson@gmail.com>
2011-10-09 10:29:03 -0400

    Archive queries by tag.

---
+++ model/archive_query.rb
@@ -0,0 +1,12 @@
+class ArchiveQuery
+  attr_accessor :tags
+
+  def initialize query_string
+    @tags = query_string.split(/_/).reject { |t| t.empty? }
+  end
+
+  def matches? post
+    @tags.empty? or ! (@tags & post.tags).empty?
+  end
+
+end
