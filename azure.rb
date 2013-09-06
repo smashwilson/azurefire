a3fc950d82d686f281b7b08afb03983233b1e155
Ash Wilson <smashwilson@gmail.com>
2013-09-06 07:37:10 -0400

    Re-enable the comment form.

--- azure.rb
+++ azure.rb
@@ -102,12 +102,11 @@ end
 # Comment post
 
 post '/:slug' do |slug|
-  # @post = JournalPost.with_slug slug
-  # halt 404 unless @post
-  # comment = Comment.new
-  # comment.name = params[:name]
-  # comment.content = params[:body]
-  # @post.add_comment comment
-  # redirect to("/#{slug}#comment-#{comment.number}")
-  redirect to("/#{slug}")
+  @post = JournalPost.with_slug slug
+  halt 404 unless @post
+  comment = Comment.new
+  comment.name = params[:name]
+  comment.content = params[:body]
+  @post.add_comment comment
+  redirect to("/#{slug}#comment-#{comment.number}")
 end
