19c942363c4d16a527449e357ae960cdc07461f4
Ash Wilson <smashwilson@gmail.com>
2014-06-11 20:34:49 -0400

    Let's try writing this plugin.

---
+++ _plugins/asset_publisher.rb
@@ -0,0 +1,55 @@
+require 'fog'
+require 'yaml'
+require 'find'
+
+FOG_CONFIG = 'fog.yml'
+
+module AssetPublisher
+  class Generator < Jekyll::Generator
+    def generate(site)
+      unless File.exist? FOG_CONFIG
+        puts "No config."
+        site.data['assets.url'] = site.data['site.url']
+        return
+      end
+
+      puts "Publishing assets!"
+      puts "Got the fog config!"
+      @fog = YAML.load_file FOG_CONFIG
+
+      @storage = Fog::Storage.new(
+        provider: :rackspace,
+        rackspace_username: @fog['account']['username'],
+        rackspace_api_key: @fog['account']['api_key']
+      )
+
+      @directory = @storage.directories.new(@fog['container'])
+      if @directory.nil?
+        @directory = @storage.directories.create(key: @fog['container'])
+      end
+
+      @directory.public = true
+      @directory.save
+
+      # Upload each file listed in 'files'
+      @fog['files'].each do |path|
+        File.open(path) do |inf|
+          @directory.files.create(key: path, body: inf)
+        end
+      end
+
+      # Recursively upload each file listed in 'directories'
+      @fog['directories'].each do |root|
+        Find.find(root) do |path|
+          next unless File.file?(path)
+
+          File.open(path) do |inf|
+            @directory.files.create(key: path, body: inf)
+          end
+        end
+      end
+
+      site.data['assets.url'] = @directory.public_url
+    end
+  end
+end
