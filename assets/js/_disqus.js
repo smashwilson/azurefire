5bb260c03ecbea7bb2356719032c0d5d5a370164
Ash Wilson <smashwilson@gmail.com>
2014-06-30 09:42:52 -0400

    Only load Disqus when there's a thread <div>.

--- assets/js/_disqus.js
+++ assets/js/_disqus.js
@@ -1,9 +1,11 @@
 // Enable Disqus comments at an <div id="disqus_thread"></div> element.
 
-(function () {
-  var dsq = document.createElement('script');
-  dsq.type = 'text/javascript';
-  dsq.async = true;
-  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
+$(function () {
+  if ($('#disqus_thread').length) {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  }
+});
