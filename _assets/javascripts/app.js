ef93b1830d3272d221a8a51e02d6406d7006944d
Ash Wilson <smashwilson@gmail.com>
2014-07-12 10:51:37 -0400

    Shuffle Javascript resources into _assets.

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,9 @@
+// Master file for Javascript resources.
+
+//= require_tree vendor
+//= require_tree plugins
+//= require nav
+//= require customizations
+//= require disqus
+
+// modernizr is included separately because it needs to be available in <head>.
