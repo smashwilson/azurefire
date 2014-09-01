824805493d2adbe7a767f833190095a371da7f87
Ash Wilson <smashwilson@gmail.com>
2014-09-01 16:08:51 -0400

    Turn off the CDN links if we're developing locally.

---
+++ _plugins/localdev.rb
@@ -0,0 +1,8 @@
+# Wipe out the site.url setting if I'm developing locally.
+module BaseUrl
+  class Generator < Jekyll::Generator
+    def generate(site)
+      site.config[:url] = nil unless File.exist?(File.join(site.source, FOG_CONFIG))
+    end
+  end
+end
