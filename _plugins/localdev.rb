666ee601d600293529804ebf924355eefcf6a778
Ash Wilson <smashwilson@gmail.com>
2014-09-01 16:15:40 -0400

    Right, string keys.

--- _plugins/localdev.rb
+++ _plugins/localdev.rb
@@ -2,7 +2,7 @@
 module BaseUrl
   class Generator < Jekyll::Generator
     def generate(site)
-      site.config[:url] = nil unless File.exist?(File.join(site.source, FOG_CONFIG))
+      site.config['url'] = nil unless File.exist?(File.join(site.source, FOG_CONFIG))
     end
   end
 end
